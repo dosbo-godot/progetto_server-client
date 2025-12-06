60d9a9bed9b8f43fb95cdc76b117e996ca2c5bb4
dosbo-godot <3d04rd0.D3lv3cchi0@gmail.com>
2025-12-06 18:35:02 +0100

    #

--- client.py
+++ client.py
@@ -1,26 +1,89 @@
 import socket
 
-def appaiamento(_):
-    print('$ COLLEGAMENTO CLIENT-CLIENT avvenuto con SUCCESSO')
+def rimanda(func) -> callable:
+    def nuovaFunc(self, *args, **kwargs):
+        risultato = func(self, *args, **kwargs)
+        self.statoFondamentale()
+        return risultato
+    return nuovaFunc
 
-def esito(args):
-    e = args[0]
-    print(f'$ PARTITA VINTA DA {e}')
+class Client:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(('127.0.0.1', PORTA))
+        print(f'\nConnessione con server su porta {PORTA} avvenuta con successo!')
+        print(f'\nIn attesa di un altro client su porta {PORTA} per appaiamento ...')
 
-eventi = {'0'   :   appaiamento,
-          '100' :   esito}
+        # TABELLA EVENTI
+        self.eventi = {'0'   :   self.appaiamento,
+                    '100'   :   self.turno,
+                    '200'   :   self.esito}
+
+    def statoFondamentale(self):
+        # ATTESA
+        data = self.client.recv(128).decode(encoding='utf-8')
+
+        data.split('/')
+        evento = data[0]
+        argomenti = data[1:]
+
+        # CHIAMO COMANDO
+        self.eventi[evento](argomenti)
+    
+    @rimanda
+    def appaiamento(self, argomenti):
+        self.marker = argomenti[0]
+        print('$ COLLEGAMENTO CLIENT-SERVER-CLIENT avvenuto con SUCCESSO')
+
+    @rimanda
+    def esito(self, argomenti):
+        e = argomenti[0]
+        print(f'$ PARTITA VINTA DA {e}')
+
+    @rimanda
+    def turno(self, argomenti):
+        griglia = argomenti[0]
+        griglia = self.estraiGriglia(griglia)
+        edit_mode = argomenti[1]
+
+        self.mostraGriglia(griglia)
+        if edit_mode:
+            mossa = self.ottieniMossa()
+            griglia = self.applicaMossa(griglia, mossa)
+            griglia = self.formattaGriglia(griglia)
+            self.client.sendall(griglia)
+        
+    def mostraGriglia(self, griglia):
+        for i, riga in enumerate(griglia):
+            riga_format = '\t|\t'.join(riga)
+            print(riga_format)
+
+            if i != (len(griglia)-1):
+                print('-'*20)
+    
+    def applicaMossa(self, griglia, mossa):
+        riga = mossa[0]
+        colonna = mossa[1]
+
+        griglia[riga][colonna] = self.marker
+    
+    def formattaGriglia(self, griglia) -> str:
+        # 0, 0, 1; 1, 2, 0; 0, 0, 0
+        formatG = []
+        for riga in griglia:
+            formatG.append(','.join(riga))
+        formatG = ';'.join(formatG)
+        return formatG
+
+    def estraiGriglia(self, grigliaF) -> list[list[str]]:
+        griglia = []
+        for rigaF in grigliaF.split(';'):
+            griglia.append(rigaF.split(','))
+        return griglia
+
+
 
 with open('ultima_porta.txt', 'r') as file:
     PORTA = int(file.read())
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', PORTA))
-print(f'\nConnessione con server su porta {PORTA} avvenuta con successo!')
-print(f'\nIn attesa di un altro client su porta {PORTA} per appaiamento ...')
-
-data = client.recv(1024).decode(encoding='utf-8')
-data.split('/')
-
-evento = data[0]
-argomenti = data[1:]
-eventi[evento](argomenti)
+c = Client()
--- server.py
+++ server.py
@@ -2,14 +2,14 @@ import socket
 
 STOP = '900/'.encode('utf-8')
 SEND = '900/1'.encode('utf-8')
-TURNO = '200/'.encode('utf-8') # griglia, editMode
-ESITO = '100/'.encode('utf-8') # 1 | vincita X ; 2 | vincita O ; 3 | pareggio
-MARKER = '0/'.encode('utf-8')
+TURNO = '200'.encode('utf-8') # griglia, editMode
+ESITO = '100'.encode('utf-8') # 1 | vincita X ; 2 | vincita O ; 3 | pareggio
+MARKER = '0'.encode('utf-8')
 
 def getPorta() -> int:
-    with open('ultima_porta.txt', 'w+') as file:
+    with open('ultima_porta.txt') as file:
         ultima_porta = file.read()
-
+    with open('ultima_porta.txt', 'w') as file:
         if ultima_porta and ultima_porta != '7999':
             PORTA = int(ultima_porta) + 1
         else:
@@ -20,6 +20,13 @@ def getPorta() -> int:
 
 def elementiUguali(lista):
     return len(set(lista)) == 1
+
+def formatMesg(codice, *args):
+    argomentiF = [str(x) for x in args]
+    argomentiF = '/'.join(argomentiF)
+    messaggio = f'{codice}/{argomentiF}'
+
+    return messaggio.encode()
 
 class Server:
     def __init__(self) -> None:
@@ -34,6 +41,8 @@ class Server:
         print(f'> Connessione con {self.ip1} avvenuta con successo!')
         self.conn2, self.ip2 = self.server.accept()
         print(f'> Connessione con {self.ip2} avvenuta con successo!')
+
+        self.assettoIniziale()
     
     def __repr__(self) -> str:
         s = f'\n\n======== SERVER SU PORTA {self.PORTA} ========'
@@ -44,10 +53,10 @@ class Server:
 
         return s+c1+p1+c2+p2
     
-    def assetoIniziale(self) -> None:
+    def assettoIniziale(self) -> None:
         # messaggio pairing riuscito
-        self.conn1.sendall(MARKER + '1')
-        self.conn2.sendall(MARKER + '2') 
+        self.conn1.sendall(formatMesg(MARKER, 1))
+        self.conn2.sendall(formatMesg(MARKER, 2))
 
         # attesa messaggio di conferma
         while True:
@@ -75,34 +84,39 @@ class Server:
         return esito
     
     def analisiGriglia(self, griglia) -> str:
-        esito = ''
+        risultato = ''
         conta_vuoti = 0
         
         for i, riga in enumerate(griglia):
             numero_vuoti = riga.count('0')
             conta_vuoti += numero_vuoti
 
-            esito = self.controllaArray(riga, esito)
+            risultato = self.controllaArray(riga, risultato)
     
             # controllo colonne
             colonna = [r[i] for r in griglia]
-            esito = self.controllaArray(colonna, esito)
+            risultato = self.controllaArray(colonna, risultato)
     
         # controllo diagonali
         diagonale1 = [griglia[k][k] for k in range(len(griglia))]
-        esito = self.controllaArray(diagonale1, esito)
+        risultato = self.controllaArray(diagonale1, risultato)
         
         diagonale2 = [griglia[k][(k*(-1))-1] for k in range(len(griglia))]
-        esito = self.controllaArray(diagonale2, esito)
+        risultato = self.controllaArray(diagonale2, risultato)
 
         # controllo pareggio
         if not conta_vuoti:
-            esito = '3'
+            risultato = '3'
 
-        print(f'$ esito: {esito}')
-        return esito
+        print(f'$ esito: {risultato}')
+        if risultato:
+            self.conn1.sendall(formatMesg(ESITO, risultato))
+            self.conn2.sendall(formatMesg(ESITO, risultato))
+        return risultato
 
     def formattaGriglia(self, griglia) -> str:
+        # 0, 0, 1; 1, 2, 0; 0, 0, 0
+        formatG = []
         for riga in griglia:
             formatG.append(','.join(riga))
         formatG = ';'.join(formatG)
@@ -111,43 +125,45 @@ class Server:
     def estraiGriglia(self, grigliaF):
         griglia = []
         for rigaF in grigliaF.split(';'):
-            griglia.append(rigaF.split(',')
+            griglia.append(rigaF.split(','))
         return griglia
 
     def gameLoop(self, griglia) -> None:
-        while game:
+        continuo = True
+        while continuo:
+            # TURNO CLIENT 1
             griglia = self.formattaGriglia(griglia)
 
-            # TURNO CLIENT 1
-            self.conn1.sendall(f'{TURNO}{griglia}/1') # invio griglia e attesa di risposta 
-            self.conn2.sendall(f'{TURNO}{griglia}/0') # invio griglia per solo display
+            self.conn1.sendall(formatMesg(TURNO, griglia, 1)) # invio griglia e attesa di risposta 
+            self.conn2.sendall(formatMesg(TURNO, griglia, 0)) # invio griglia per solo display
             
-            griglia = self.conn1.recv(1024).decode()
-            self.analisiGriglia(griglia)
-            griglia = self.formattaGriglia(griglia)
+            griglia = self.conn1.recv(128).decode()
+            griglia  = self.estraiGriglia(griglia)
+            game = self.analisiGriglia(griglia)
+            if game: break
 
             # TURNO CLIENT 2
-            self.conn2.sendall(TURNO + griglia)
-            griglia = self.conn2.recv(1024).decode()
             griglia = self.formattaGriglia(griglia)
-            self.conn1.sendall(griglia)
-        
-# dopo collegamento client aspettano uno stato : STOP o SEND
-# STOP: solo ascolto
-# SEND: acquisizione ed invio di mossa del giocatore
-# nello stato STOP il client riceve un dizionario che ridefinisce le sue variabili
-# es. grid : [X, O, None, ...]
 
-# MAIN
+            self.conn1.sendall(formatMesg(TURNO, griglia, 0)) # invio griglia e attesa di risposta 
+            self.conn2.sendall(formatMesg(TURNO, griglia, 1)) # invio griglia per solo display
+            
+            griglia = self.conn2.recv(128).decode()
+            griglia  = self.estraiGriglia(griglia)
+            game = self.analisiGriglia(griglia)
+            if game: break
 
-# self.conn2.sendall(ESITO + vincitore)
+            nuovo_turno = input(f'esegurie turno?\n>> ')
+            continuo = bool(nuovo_turno)
+        self.arresta()
+    
+    def arresta(self):
+        print('$ partita finita')
+        self.conn1.close()
+        self.conn2.close()
+        self.server.close()
 
+# main
 
-
-'''gioco = True
-while gioco:
-    conn1.sendall(SEND)
-    mossa1 = conn1.recv(1024)
-    conn1.sendall(STOP)
-    conn2.sendall(mossa1)'''
-
+s = Server()
+print(s)
